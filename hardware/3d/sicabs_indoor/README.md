fa28cfeb7ccc990bfdfd77e21a5dc3a2b9f85d9a
Sebastián García <isebastian.ga@gmail.com>
2023-08-04 15:53:28 -0500

    init folder structure

--- hardware/3d/sicabs_indoor/README.md
+++ hardware/3d/sicabs_indoor/README.md
@@ -1,3 +1,3 @@
-# SICABS
+## SICABS
 
 Sistema de Control de Acceso Seguro utilizando Autenticación Biométrica y Criptografía
---
+++ src/sicabs_outdoor/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 2
+#endif
+
+void setup() {
+    // put your setup code here, to run once:
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
