cb8276a09e59db420f59da4c76e4e423109978e1
Sebastián García <isebastian.ga@gmail.com>
2023-11-07 08:22:42 -0500

    Update keypad pin configuration in config.h

--- src/sicabs_outdoor/config.h
+++ src/sicabs_outdoor/config.h
@@ -17,8 +17,8 @@ char KEYPAD_KEYS[KEYPAD_ROWS][KEYPAD_COLS] = {
     {'4', '5', '6'},
     {'7', '8', '9'},
     {'*', '0', '#'}};
-byte rowPins[KEYPAD_ROWS] = {1, 7, 6, 4};  // ROW1 -> P1, ROW2 -> P7, ROW3 -> P6, ROW4 -> P4
-byte colPins[KEYPAD_COLS] = {2, 0, 5};     // COL1 -> P2, COL2 -> P0, COL3 -> P5
+byte rowPins[KEYPAD_ROWS] = {1, 7, 6, 4};  // ROW0 -> P1, ROW1 -> P7, ROW2 -> P6, ROW3 -> P4
+byte colPins[KEYPAD_COLS] = {2, 0, 5};     // COL0 -> P2, COL1 -> P0, COL2 -> P5
 
 #define KEYPAD_DEBOUNCE_TIME_MS 10  // Default by library is 10
 #define KEYPAD_HOLD_TIME_MS 500     // Default by library is 500
