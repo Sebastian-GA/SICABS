d9eaf6c83180b2c1da279aa23322792b407f1450
CamiloTello002 <camilojtello5@gmail.com>
2024-02-26 18:42:26 -0500

    Refactoring with classes

--- src/sicabs_outdoor/main.cpp
+++ src/sicabs_outdoor/main.cpp
@@ -8,19 +8,7 @@ TaskHandle_t video;
 
 void setup() {
     Serial.begin(115200);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-    Serial.println();
-    Serial.println("---SICABS Test---");
-    Serial.println();
-
-    if (xTaskCreatePinnedToCore(videoTransmission, "Video transmission", 100000, NULL, 2, &video, 1) != pdPASS) {
-        Serial.println("video not allocated :(");
-    }
-
-    vTaskDelete(NULL);
 }
 
 void loop() {
-    vTaskDelete(NULL);
 }
